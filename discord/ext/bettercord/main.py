a4ac7abbf6e5d4520e00668b757dff19fe010649
That GenZ Gamer <78960084+ThatGenZGamer48@users.noreply.github.com>
2022-03-06 16:02:10 +0530

    Change func name. Add get_text_help

--- discord/ext/bettercord/main.py
+++ discord/ext/bettercord/main.py
@@ -4,12 +4,11 @@ from typing import Union
 
 class BetterCord:
     # TODO: Add discord.Bot too.
-    def __init__(self, pycord_bot: commands.Bot, developer_mode: bool = False, help_type: list = "embed"):
+    def __init__(self, pycord_bot: commands.Bot, developer_mode: bool = False):
         self.pycord_bot: commands.Bot = pycord_bot
         self.developer_mode: bool = developer_mode
-        self.help_type: list = help_type
         
-    def create_help_embed(self, embed_title: str = None, embed_colour: discord.Colour = None):
+    def get_embed_help(self, embed_title: str = None, embed_colour: discord.Colour = None):
         cog_names = [cog for cog in self.pycord_bot.cogs]
         
         emb_title = "Help"
@@ -38,4 +37,28 @@ class BetterCord:
             embed.add_field(name=cog_name, value=command_list, inline=False)
 
         return embed
+    
+    def get_text_help(self, text_title: str = None):
+        cog_names = [cog for cog in self.pycord_bot.cogs]
+        
+        final_text: str = ""
+        
+        txt_title: str = "**Help**"
+        
+        if text_title:
+            txt_title: str = f"**{text_title}**"
+                
+        final_text += txt_title
+        
+        for cog_name in cog_names:
+            commands = []
+
+            for command in self.pycord_bot.get_cog(cog_name).walk_commands():
+                commands.append(f'`{command.name}`')
+
+            command_list = ", ".join(commands)
+            
+            final_text += f"\n\n**{cog_name}**\n{command_list}"
+
+        return final_text
         
