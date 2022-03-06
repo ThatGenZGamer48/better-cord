4d1d7f079c1323d8db90c2e330d31378a782b25d
That GenZ Gamer <78960084+ThatGenZGamer48@users.noreply.github.com>
2022-03-06 15:42:35 +0530

    Create initialize file.

---
+++ discord/ext/bettercord/__init__.py
@@ -0,0 +1,10 @@
+"""
+A module designed to make coding in Pycord easier.
+By That GenZ Gamer
+"""
+
+from .main import *
+
+__author__ = "That GenZ Gamer"
+__license__ = "MIT"
+__version__ = "0.0.1"
